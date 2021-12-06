5e2eaafab8ca941dd8abda0c3d3bc1a419483099
Swiiptz <milan.etudes@gmail.com>
2021-12-06 21:08:58 +0100

    Maj

---
+++ Test-C-/Test1.cpp
@@ -0,0 +1,11 @@
+#include <iostream> //charge la fonctionnalité bibliothèque
+
+using namespace std;
+
+int main()
+{
+    int const nombreNiveaux(10);
+    string const motDePasse("w6p4Yui2");
+    double const pi(3.14);
+    unsigned int const pointsDeVieMaximum(100);
+}
